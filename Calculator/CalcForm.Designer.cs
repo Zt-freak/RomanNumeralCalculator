2f3aa3bac6861a879e7d61246252274d8a63db73
Zt-freak <bnkho98@hotmail.com>
2020-12-28 20:28:51 +0100

    copy code of calculator

---
+++ Calculator/CalcForm.Designer.cs
@@ -0,0 +1,237 @@
+﻿using System.Collections;
+
+namespace Calculator
+{
+    partial class CalcForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.display = new System.Windows.Forms.TextBox();
+            this.digitButtons = new ArrayList();
+            for (int i = 0; i < 12; i++)
+            {
+                this.digitButtons.Add(new DigitButton());
+            }
+            this.operatorButtonPlus = new System.Windows.Forms.Button();
+            this.operatorButtonMinus = new System.Windows.Forms.Button();
+            this.operatorButtonMultiply = new System.Windows.Forms.Button();
+            this.operatorButtonDivide = new System.Windows.Forms.Button();
+            this.operatorButtonPercentage = new System.Windows.Forms.Button();
+            this.answerButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.euroButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // display
+            // 
+            this.display.Location = new System.Drawing.Point(13, 12);
+            this.display.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.display.Name = "display";
+            this.display.ReadOnly = true;
+            this.display.Size = new System.Drawing.Size(315, 22);
+            this.display.TabIndex = 0;
+            this.display.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Answer_Selector);
+
+            //
+            // digitButtons
+            //
+            int ButtonIndex = 0;
+            int tempDigitButtonColumn = 0;
+            int tempDigitButtonRow = 0;
+            foreach (DigitButton digitButton in this.digitButtons)
+            {
+                if ((ButtonIndex % 3) == 0)
+                {
+                    tempDigitButtonColumn = 0;
+                    tempDigitButtonRow++;
+                }
+                if (ButtonIndex == 10)
+                {
+                    digitButton.Text = ".";
+                    digitButton.Enabled = false;
+                }
+                else if (ButtonIndex == 11)
+                {
+                    digitButton.Text = "-";
+                }
+                else {
+                    digitButton.Text = ButtonIndex.ToString();
+                }
+                digitButton.Location = new System.Drawing.Point(13 + (108 * tempDigitButtonColumn), 6 + (36 * tempDigitButtonRow));
+                digitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+                digitButton.Name = "digitButton" + ButtonIndex.ToString();
+                digitButton.Size = new System.Drawing.Size(100, 28);
+                digitButton.TabIndex = 1 + ButtonIndex;
+                digitButton.UseVisualStyleBackColor = true;
+                digitButton.Click += new System.EventHandler(this.Digit_Click);
+                ButtonIndex++;
+                tempDigitButtonColumn++;
+            }
+
+            // 
+            // operatorButtonPlus
+            // 
+            this.operatorButtonPlus.Location = new System.Drawing.Point(13, 187);
+            this.operatorButtonPlus.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.operatorButtonPlus.Name = "operatorButtonPlus";
+            this.operatorButtonPlus.Size = new System.Drawing.Size(100, 28);
+            this.operatorButtonPlus.TabIndex = ButtonIndex++;
+            this.operatorButtonPlus.Text = "+";
+            this.operatorButtonPlus.UseVisualStyleBackColor = true;
+            this.operatorButtonPlus.Click += new System.EventHandler(this.Operator_Click);
+            this.operatorButtonPlus.Enabled = false;
+            // 
+            // operatorButtonMinus
+            // 
+            this.operatorButtonMinus.Location = new System.Drawing.Point(121, 187);
+            this.operatorButtonMinus.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.operatorButtonMinus.Name = "operatorButtonMinus";
+            this.operatorButtonMinus.Size = new System.Drawing.Size(100, 28);
+            this.operatorButtonMinus.TabIndex = ButtonIndex++;
+            this.operatorButtonMinus.Text = "-";
+            this.operatorButtonMinus.UseVisualStyleBackColor = true;
+            this.operatorButtonMinus.Click += new System.EventHandler(this.Operator_Click);
+            this.operatorButtonMinus.Enabled = false;
+            // 
+            // operatorButtonMultiply
+            // 
+            this.operatorButtonMultiply.Location = new System.Drawing.Point(229, 187);
+            this.operatorButtonMultiply.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.operatorButtonMultiply.Name = "operatorButtonMultiply";
+            this.operatorButtonMultiply.Size = new System.Drawing.Size(100, 28);
+            this.operatorButtonMultiply.TabIndex = ButtonIndex++;
+            this.operatorButtonMultiply.Text = "*";
+            this.operatorButtonMultiply.UseVisualStyleBackColor = true;
+            this.operatorButtonMultiply.Click += new System.EventHandler(this.Operator_Click);
+            this.operatorButtonMultiply.Enabled = false;
+            // 
+            // operatorButtonDivide
+            // 
+            this.operatorButtonDivide.Location = new System.Drawing.Point(337, 187);
+            this.operatorButtonDivide.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.operatorButtonDivide.Name = "operatorButtonDivide";
+            this.operatorButtonDivide.Size = new System.Drawing.Size(100, 28);
+            this.operatorButtonDivide.TabIndex = ButtonIndex++;
+            this.operatorButtonDivide.Text = "/";
+            this.operatorButtonDivide.UseVisualStyleBackColor = true;
+            this.operatorButtonDivide.Click += new System.EventHandler(this.Operator_Click);
+            this.operatorButtonDivide.Enabled = false;
+            // 
+            // operatorButtonPercentage
+            // 
+            this.operatorButtonPercentage.Location = new System.Drawing.Point(445, 187);
+            this.operatorButtonPercentage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.operatorButtonPercentage.Name = "operatorButtonDivide";
+            this.operatorButtonPercentage.Size = new System.Drawing.Size(100, 28);
+            this.operatorButtonPercentage.TabIndex = ButtonIndex++;
+            this.operatorButtonPercentage.Text = "%";
+            this.operatorButtonPercentage.UseVisualStyleBackColor = true;
+            this.operatorButtonPercentage.Click += new System.EventHandler(this.Operator_Click);
+            this.operatorButtonPercentage.Enabled = false;
+
+            //
+            // answerButton
+            //
+            this.answerButton.Location = new System.Drawing.Point(13, 225);
+            this.answerButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.answerButton.Name = "answerButton";
+            this.answerButton.Size = new System.Drawing.Size(100, 28);
+            this.answerButton.TabIndex = ButtonIndex++;
+            this.answerButton.Text = "=";
+            this.answerButton.UseVisualStyleBackColor = true;
+            this.answerButton.Click += new System.EventHandler(this.Answer_Click);
+            this.answerButton.Enabled = false;
+
+            //
+            // euroButton
+            //
+            this.euroButton.Location = new System.Drawing.Point(121, 225);
+            this.euroButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.euroButton.Name = "euroButton";
+            this.euroButton.Size = new System.Drawing.Size(100, 28);
+            this.euroButton.TabIndex = ButtonIndex++;
+            this.euroButton.Text = "€";
+            this.euroButton.UseVisualStyleBackColor = true;
+            this.euroButton.Click += new System.EventHandler(this.Answer_Click);
+            this.euroButton.Enabled = false;
+
+            //
+            // clearButton
+            //
+            this.clearButton.Location = new System.Drawing.Point(229, 225);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(100, 28);
+            this.clearButton.TabIndex = ButtonIndex++;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.Clear_Click);
+
+            // 
+            // CalcForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 554);
+            this.Controls.Add(this.operatorButtonPercentage);
+            this.Controls.Add(this.operatorButtonDivide);
+            this.Controls.Add(this.operatorButtonMultiply);
+            this.Controls.Add(this.operatorButtonMinus);
+            this.Controls.Add(this.operatorButtonPlus);
+            this.Controls.Add(this.answerButton);
+            this.Controls.Add(this.euroButton);
+            this.Controls.Add(this.clearButton);
+
+            foreach (DigitButton digitButton in this.digitButtons)
+            {
+                this.Controls.Add(digitButton);
+            }
+            this.Controls.Add(this.display);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "CalcForm";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.CalcForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox display;
+        private System.Collections.ArrayList digitButtons;
+        private System.Windows.Forms.Button operatorButtonPlus;
+        private System.Windows.Forms.Button operatorButtonMinus;
+        private System.Windows.Forms.Button operatorButtonMultiply;
+        private System.Windows.Forms.Button operatorButtonDivide;
+        private System.Windows.Forms.Button operatorButtonPercentage;
+        private System.Windows.Forms.Button answerButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button euroButton;
+        // TODO: Add element for inserting previous answers
+    }
+}
+
---
+++ Calculator/CalcForm.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Calculator
+{
+    public partial class CalcForm : Form
+    {
+        public float firstValue = 0;
+        public float secondValue = 0;
+        public System.Collections.ArrayList memory;
+        public string mathOperator = "";
+        public int step = 0;
+        public int memoryPointer = 0;
+        public CalcForm()
+        {
+            this.memory = new System.Collections.ArrayList();
+            InitializeComponent();
+        }
+
+        private void CalcForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Digit_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            switch (this.step)
+            {
+                case 0:
+                    this.display.Text = button.Text;
+                    if (button.Text != "-")
+                    {
+                        this.step++;
+                        this.enableOperatorOptions(true);
+                    }
+                    break;
+                case 2:
+                    this.display.Text += button.Text;
+                    this.enableOperatorOptions(false);
+                    this.enableAnswerOptions(true);
+                    this.step++;
+                    break;
+                case 1:
+                case 3:
+                    this.display.Text += button.Text;
+                    break;
+                case 4:
+                    this.display.Text = button.Text;
+                    {
+                        this.step = 1;
+                        this.enableOperatorOptions(true);
+                    }
+                    break;
+            }
+            this.checkDigitOptions();
+            //Debug.WriteLine(button.Text);
+            Debug.WriteLine(this.step);
+        }
+
+        private void Operator_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            switch (this.step)
+            {
+                case 1:
+                    this.firstValue = float.Parse(this.display.Text);
+                    this.mathOperator = button.Text;
+                    this.display.Text = "";
+                    this.step++;
+                    break;
+                case 4:
+                    this.firstValue = float.Parse(this.display.Text);
+                    this.mathOperator = button.Text;
+                    this.display.Text = "";
+                    this.step = 2;
+                    break;
+                default:
+                    this.mathOperator = button.Text;
+                    break;
+            }
+            Debug.WriteLine(this.step);
+        }
+
+        private void Answer_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            switch (this.step)
+            {
+                case 3:
+                    this.secondValue = float.Parse(this.display.Text);
+                    this.display.Text = "";
+                    float answer = 0;
+                    switch (this.mathOperator)
+                    {
+                        case "+":
+                            answer = this.firstValue + this.secondValue;
+                            break;
+                        case "-":
+                            answer = this.firstValue - this.secondValue;
+                            break;
+                        case "*":
+                            answer = this.firstValue * this.secondValue;
+                            break;
+                        case "/":
+                            answer = this.firstValue / this.secondValue;
+                            break;
+                        case "%":
+                            answer = (this.secondValue - this.firstValue) / this.firstValue * 100;
+                            break;
+                    }
+                    this.memory.Add(answer);
+                    if (button.Text == "€")
+                    {
+                        this.display.Text = answer.ToString("c2");
+                    }
+                    else
+                    {
+                        this.display.Text = answer.ToString();
+                    }
+                    this.step = 4;
+                    this.checkDigitOptions();
+                    this.enableOperatorOptions(true);
+                    this.enableAnswerOptions(false);
+                    this.memoryPointer = this.memory.Count - 1;
+                    break;
+            }
+            Debug.WriteLine(this.step);
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            this.display.Text = "";
+            this.step = 0;
+            this.checkDigitOptions();
+            this.enableOperatorOptions(false);
+            Debug.WriteLine(this.step);
+        }
+
+        private void Answer_Selector(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                if (this.memoryPointer > 0)
+                {
+                    this.memoryPointer--;
+                }
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                if (this.memoryPointer < this.memory.Count - 1)
+                {
+                    this.memoryPointer++;
+                }
+            }
+            int tempPointer = 0;
+            foreach (float answer in this.memory)
+            {
+                if (tempPointer == this.memoryPointer)
+                {
+                    this.display.Text = answer.ToString();
+                }
+            }
+        }
+
+        private void enableOperatorOptions (Boolean value)
+        {
+            this.operatorButtonPlus.Enabled = value;
+            this.operatorButtonMinus.Enabled = value;
+            this.operatorButtonMultiply.Enabled = value;
+            this.operatorButtonDivide.Enabled = value;
+            this.operatorButtonPercentage.Enabled = value;
+        }
+
+        private void enableAnswerOptions(Boolean value)
+        {
+            this.answerButton.Enabled = value;
+            this.euroButton.Enabled = value;
+        }
+
+        private void checkDigitOptions()
+        {
+            int tempDigitButtonIndex = 0;
+            foreach (DigitButton digitButton in this.digitButtons)
+            {
+                if (this.display.TextLength >= 1 && digitButton.Text == "-")
+                {
+                    digitButton.Enabled = false;
+                }
+                else if (digitButton.Text == "-")
+                {
+                    digitButton.Enabled = true;
+                }
+                if (this.display.TextLength >= 1 && this.display.Text.Contains('.') && digitButton.Text == "."
+                    || this.display.TextLength == 0 && digitButton.Text == "."
+                    || digitButton.Text == "." && this.display.Text == "-"
+                    || digitButton.Text == "." && this.step == 4)
+                {
+                    digitButton.Enabled = false;
+                }
+                else if (digitButton.Text == ".")
+                {
+                    digitButton.Enabled = true;
+                }
+                tempDigitButtonIndex++;
+            }
+        }
+    }
+}
